06b115abf8c568b086a3a0c60fb0f24c5d21791f
mirqodirov2001 <mirqodirov678@gmail.com>
2023-10-03 10:58:42 +0500

    Timer App

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import TimerApp from "./components/Timer App"
+const App = () => {
+  return (
+    <div className="container">
+  <TimerApp/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Timer App/index.jsx
@@ -0,0 +1,7 @@
+const Timer = () => {
+  return (
+    <div className=" text-blue-700 text-[200px] text-center">Timer App  </div>
+  )
+}
+
+export default Timer
